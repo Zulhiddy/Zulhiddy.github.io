8980aac09118154271a709c4d4b872bbae8766eb
unknown <muhdzulhilmisapari@gmail.com>
2018-12-01 16:21:39 +0800

    Java

---
+++ day5/home.js
@@ -0,0 +1,41 @@
+var my_name;
+var my_age;
+var my_birthdate; 
+
+my_name = "stackup";
+my_age = 25;
+my_birthdate = "15/02/1993";
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthdate);
+
+var my_address = "JTC Launchpad";
+
+var my_test;
+
+console.log( typeof(my_test) );
+console.log( typeof(my_name) );
+console.log( typeof(my_age) );
+
+var firstName;
+var lastName;
+var space;
+
+firstName = "Zul";
+lastName = "Sapari";
+space = " ";
+
+var fullName = firstName + space + lastName;
+var fullName = firstName + " " + lastName
+
+console.log(fullName);
+console.log(fullName);
+
+var qty = 2;
+var total = 200;
+
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
+
